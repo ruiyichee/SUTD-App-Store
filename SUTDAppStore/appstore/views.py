6d7e67dabc5a208d8d5901638ebbd2d61bdbb627
MatthewNeo <matthew_neo@mymail.sutd.edu.sg>
2017-11-07 02:11:50 +0800

    Add files via upload

--- SUTDAppStore/appstore/views.py
+++ SUTDAppStore/appstore/views.py
@@ -7,7 +7,12 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import App
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.shortcuts import render, redirect, render_to_response
+from appstore.forms import SignUpForm, EmailChangeForm
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 
 
 # Create your views here.
@@ -37,3 +42,46 @@ def detail(request, app_id):
 class DetailView(generic.DetailView):
     model = App
     template_name = 'appstore/detail.html'
+	
+def signup(request):
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return redirect('/login')
+	else:
+		form = SignUpForm()
+	return render(request, 'signup.html', {'form':form})
+
+def change_password(request):
+	if request.method == "POST":
+		form = PasswordChangeForm(request.user, request.POST)
+		if form.is_valid():
+			user = form.save()
+			update_session_auth_hash(request, user)
+			messages.success(request, 'Your password was successfully updated!')
+			return redirect('/appstore')
+		else:
+			messages.error(request, 'Please correct the error below.')
+	else:
+		form = PasswordChangeForm(request.user)
+	return render(request, 'accounts/change_password.html', {'form': form})
+
+def edit_particulars(request):
+	#form = EmailChangeForm(request.user)
+	if request.method=='POST':
+		form = EmailChangeForm(request.user, request.POST)
+		if form.is_valid():
+			user = form.save()
+			update_session_auth_hash(request, user)
+			messages.success(request, 'Your email was successfully updated!')
+			return redirect('/appstore')
+		else:
+			messages.error(request, 'Please correct the error below.')
+	else:
+		form = EmailChangeForm(request.user)
+	return render(request, 'accounts/profile/edit_particulars.html', {'form': form})
