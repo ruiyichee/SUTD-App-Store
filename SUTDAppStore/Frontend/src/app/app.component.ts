5b360dde09c8281ad10b24e7d28d033690759b97
yinjisheng <jisheng_yin@mymail.sutd.edu.sg>
2017-11-12 09:37:47 +0800

    Frontend angular 2

---
+++ SUTDAppStore/Frontend/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { App } from './app.model';
+import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  url: string = 'http://127.0.0.1:8000/appstore';
+  appList = [];
+  constructor(private http: Http) { }
+  
+  public getApps() {
+    this.http.get(this.url).toPromise().then((res) => {
+      this.appList = [];
+      console.log(res.json().length);
+      const jsonArray = res.json();
+      for (let i = 0; i < jsonArray.length; i++) {
+        let localApp = new App();
+        localApp = jsonArray[i];
+        this.appList.push(localApp)     
+      }
+      console.log(this.appList);
+    });
+
+  }
+}
